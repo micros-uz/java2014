8879660117914e35c6b05d411109569ad0e8390f
Artem <micros-uz@ucd.uz>
2014-06-18 00:56:50 +0500

    Added: Level 1 - 01 lesson

---
+++ Level1/01/Test/build.cmd
@@ -0,0 +1,5 @@
+rem - uncomment if path variable is not set "c:\Program Files\Java\jdk1.7.0_45\bin\javac.exe" Test.java
+
+javac Test.java
+
+pause
---
+++ Level1/01/Test/run.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem "c:\Program Files\Java\jdk1.7.0_45\bin\java.exe" Test
+
+java Test
+
+pause
